745329c66fd63f0c0bb3763ff79d09b2dc330473
nicolasv112 <nicolasvillarroel6@gmail.com>
2024-02-27 17:59:25 -0300

    cambie la descripcion del sitio

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  author: "Nicolas Villarroel",
+  desc: "Facilitar la busqueda laboral de los estudiantes",
+  title: "Web laboral",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
