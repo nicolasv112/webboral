82f4fad5c8dd32fb49504d4cf813357a7d81430c
nicolasv112 <nicolasvillarroel6@gmail.com>
2024-02-27 19:31:27 -0300

    configure ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,22 @@
 import { SITE } from "@config";
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
+const propuestas = defineCollection({
   type: "content",
   schema: ({ image }) =>
     z.object({
       author: z.string().default(SITE.author),
       pubDatetime: z.date(),
-      modDatetime: z.date().optional().nullable(),
       title: z.string(),
-      featured: z.boolean().optional(),
-      draft: z.boolean().optional(),
+      modalidad: z.string(),
+      ubicacion: z.string(),
+      disponibilidad: z.string(),
+      vacantes: z.number(),
+      empresa: z.string(),
       tags: z.array(z.string()).default(["others"]),
-      ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
-          message: "OpenGraph image must be at least 1200 X 630 pixels!",
-        })
-        .or(z.string())
-        .optional(),
+
       description: z.string(),
-      canonicalURL: z.string().optional(),
     }),
 });
 
-export const collections = { blog };
+export const collections = { propuestas };
